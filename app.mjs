3ff99ed7b551f4f22b1aaf85df6b7fe15b4abbb8
Joe <chanaphat.sip@gmail.com>
2025-03-16 17:31:46 +0700

    express assignment

--- app.mjs
+++ app.mjs
@@ -7,6 +7,15 @@ app.get("/test", (req, res) => {
     return res.json("Server API is Working");
 });
 
+app.get("/profiles", (req, res) => {
+    return res.json({
+        "data":  {
+            "name": "john",
+            "age": 20
+        },
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
